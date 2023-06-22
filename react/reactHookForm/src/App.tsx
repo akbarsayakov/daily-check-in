9f465a0692e6f2f71fe85b610b1fcecf73242e29
Akbar Sayakov <akbarsayakov@gmail.com>
2023-06-21 17:17:16 -0700

    practicing react-hook-form and added devtool

---
+++ react/reactHookForm/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import YouTubeForm from './components/YouTubeForm'
+
+function App() {
+  return (
+    <>
+      <YouTubeForm />
+    </>
+  )
+}
+
+export default App
